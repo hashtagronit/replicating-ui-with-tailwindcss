81a494e32e2c5bf838d044ac25d2ab3071b5ac61
Ronit Raj <ronit9450@gmail.com>
2025-12-09 22:58:53 +0530

    heroSectionTillLandingImages

--- landing-page/README.md
+++ landing-page/README.md
@@ -1,5 +1,3 @@
 so in this commit i have done:
 
-1. created navbar component 
-2. then imported/init shadcn -neutral theme,  this updates globals.css, created components.json for storing basic settings
-3. theen imported shadcn button,  in ui folder by default
+1. 
--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // to import images from this domain
+  images: { 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com', 
+        port: '', // Leave blank unless a specific port is required
+        pathname: '/**', // Allows any path on this hostname
+      },
+    ],
+}
+}
 
 export default nextConfig;
